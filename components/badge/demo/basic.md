659077edfcb82a5869e4605ea3e5b9e9be7d724f
huiop368 <negativemh@gmail.com>
2017-02-17 17:46:23 +0800

    增加角标beta

--- components/badge/demo/basic.md
+++ components/badge/demo/basic.md
@@ -13,9 +13,25 @@ class BadgeExample extends React.Component {
   render() {
     return (
         <div>
+            <p style={{height : 20}} />
             <Badge text={100} />
+            <Badge text="角标" corner>
+                <span style={{ width: '60px', height: '60px', background: '#ddd', display: 'inline-block' }} />
+            </Badge>
             <Badge dot>
                 <span style={{ width: '26px', height: '26px', background: '#ddd', display: 'inline-block' }} />
+            </Badge>
+            <p />
+            <Badge text={2}>
+                <span style={{ width: '26px', height: '26px', background: '#ddd', display: 'inline-block' }} />
+            </Badge>
+            <p />
+            <Badge text="角标" corner>
+                <span style={{ width: '80px', height: '60px', background: '#ddd', display: 'inline-block' }} />
+            </Badge>
+            <p />
+            <Badge text="角标" corner>
+                <span style={{ width: '90px', height: '90px', background: '#ddd', display: 'inline-block' }} />
             </Badge>
         </div>
     )
--- components/badge/index.js
+++ components/badge/index.js
@@ -8,27 +8,46 @@ export default class Badge extends Component {
         className   : PropTypes.string,
         text        : PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
         dot         : PropTypes.bool,
+        corner      : PropTypes.bool,       // 试验属性，正方形精度高
         overflowCount : PropTypes.number
     };
 
     static defaultProps = {
         prefixCls       : 't-badge',
         dot             : false,
+        corner          : false,
         overflowCount   : 99
     };
 
+    componentDidMount (){
+        if(this.props.corner){
+            setTimeout( () => {
+                const width = this.refs.badge.offsetWidth
+                const height = this.refs.badge.offsetHeight
+                const sup = this.refs.sup
+                const h = Math.ceil((height/width) * (width/4))
+
+                sup.style.width = `${width}px`
+                sup.style.top = `${height/2-9-h}px`
+                sup.style.right = `-${width/4}px`
+            }, 30)
+        }
+    }
+
     render (){
-        let { prefixCls, className, style, text, dot, overflowCount, children } = this.props
+        let { prefixCls, className, style, text, dot, corner, overflowCount, children } = this.props
 
         const allCls = classnames({
             [prefixCls] : true,
             [className] : !!className,
-            [`${prefixCls}-not-a-wrapper`]: !children
+            [`${prefixCls}-not-a-wrapper`]: !children,
+            [`${prefixCls}-corner-wrapper`] : corner
         })
 
         const supCls = classnames({
             [`${prefixCls}-dot`] : dot,
-            [`${prefixCls}-text`] : !dot
+            [`${prefixCls}-text`] : !dot && !corner,
+            [`${prefixCls}-corner`] : corner
         })
 
         text = text > overflowCount ? `${overflowCount}+` : text
@@ -40,9 +59,9 @@ export default class Badge extends Component {
         const hidden = (!text || text === '0') && !dot
 
         return (
-            <div className={allCls} title={text}>
+            <div className={allCls} title={text} ref="badge">
                 { children }
-                { !hidden && <sup className={supCls} style={style}>{text}</sup>}
+                { !hidden && <sup className={supCls} style={style} ref="sup">{text}</sup>}
             </div>        
         )
     }
